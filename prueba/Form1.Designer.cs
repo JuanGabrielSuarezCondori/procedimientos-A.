d839317716223e8ff05e477a34f159e7b2becae9
Usuario <Usuario@EPIS>
2025-04-11 12:38:54 -0500

    Agregar archivos de proyecto.

---
+++ prueba/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.TBCodigo = new System.Windows.Forms.TextBox();
+            this.TBPaterno = new System.Windows.Forms.TextBox();
+            this.TBMaterno = new System.Windows.Forms.TextBox();
+            this.TBNombre = new System.Windows.Forms.TextBox();
+            this.TBUsuario = new System.Windows.Forms.TextBox();
+            this.TB = new System.Windows.Forms.TextBox();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnlistar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(353, 218);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.TabIndex = 0;
+            this.btnagregar.Text = "agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "actualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(548, 218);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 2;
+            this.btneliminar.Text = "eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // TBCodigo
+            // 
+            this.TBCodigo.Location = new System.Drawing.Point(404, 32);
+            this.TBCodigo.Name = "TBCodigo";
+            this.TBCodigo.Size = new System.Drawing.Size(100, 20);
+            this.TBCodigo.TabIndex = 4;
+            this.TBCodigo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TBPaterno
+            // 
+            this.TBPaterno.Location = new System.Drawing.Point(404, 58);
+            this.TBPaterno.Name = "TBPaterno";
+            this.TBPaterno.Size = new System.Drawing.Size(100, 20);
+            this.TBPaterno.TabIndex = 5;
+            // 
+            // TBMaterno
+            // 
+            this.TBMaterno.Location = new System.Drawing.Point(404, 84);
+            this.TBMaterno.Name = "TBMaterno";
+            this.TBMaterno.Size = new System.Drawing.Size(100, 20);
+            this.TBMaterno.TabIndex = 6;
+            // 
+            // TBNombre
+            // 
+            this.TBNombre.Location = new System.Drawing.Point(404, 110);
+            this.TBNombre.Name = "TBNombre";
+            this.TBNombre.Size = new System.Drawing.Size(100, 20);
+            this.TBNombre.TabIndex = 7;
+            // 
+            // TBUsuario
+            // 
+            this.TBUsuario.Location = new System.Drawing.Point(404, 136);
+            this.TBUsuario.Name = "TBUsuario";
+            this.TBUsuario.Size = new System.Drawing.Size(100, 20);
+            this.TBUsuario.TabIndex = 8;
+            // 
+            // TB
+            // 
+            this.TB.Location = new System.Drawing.Point(404, 163);
+            this.TB.Name = "TB";
+            this.TB.Size = new System.Drawing.Size(100, 20);
+            this.TB.TabIndex = 9;
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Location = new System.Drawing.Point(128, 260);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.Size = new System.Drawing.Size(622, 178);
+            this.dgvResultado.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Codigo Alumno:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "apellido paterno:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "apellido materno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(311, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "nombre:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "usuario:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(313, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "carrera:";
+            // 
+            // btnlistar
+            // 
+            this.btnlistar.Location = new System.Drawing.Point(257, 218);
+            this.btnlistar.Name = "btnlistar";
+            this.btnlistar.Size = new System.Drawing.Size(75, 23);
+            this.btnlistar.TabIndex = 17;
+            this.btnlistar.Text = "listar";
+            this.btnlistar.UseVisualStyleBackColor = true;
+            this.btnlistar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnlistar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvResultado);
+            this.Controls.Add(this.TB);
+            this.Controls.Add(this.TBUsuario);
+            this.Controls.Add(this.TBNombre);
+            this.Controls.Add(this.TBMaterno);
+            this.Controls.Add(this.TBPaterno);
+            this.Controls.Add(this.TBCodigo);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnagregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox TBCodigo;
+        private System.Windows.Forms.TextBox TBPaterno;
+        private System.Windows.Forms.TextBox TBMaterno;
+        private System.Windows.Forms.TextBox TBNombre;
+        private System.Windows.Forms.TextBox TBUsuario;
+        private System.Windows.Forms.TextBox TB;
+        private System.Windows.Forms.DataGridView dgvResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnlistar;
+    }
+}
+
---
+++ prueba/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+namespace prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static private string cadena = "server=dataepis.uandina.pe,49157;Database=BDAcademicoSuarez;Uid=suarezj;pwd=juan1999";
+        static private SqlConnection conexion = new SqlConnection(cadena);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                // Obtener los valores desde los TextBox
+                string codAlumno = TBCodigo.Text;
+                string aPaterno = TBPaterno.Text;
+                string aMaterno = TBMaterno.Text;
+                string nombres = TBNombre.Text;
+                string usuario = TBUsuario.Text;
+                string codCarrera = TB.Text;
+
+                // Nombre del procedimiento almacenado
+                string consulta = "spActualizarAlumno";
+
+                try
+                {
+                    using (SqlConnection conexion = new SqlConnection(cadena))
+                    {
+                        SqlCommand cmd = new SqlCommand(consulta, conexion);
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Agregar los parámetros
+                        cmd.Parameters.AddWithValue("@CodAlumno", codAlumno);
+                        cmd.Parameters.AddWithValue("@APaterno", aPaterno);
+                        cmd.Parameters.AddWithValue("@AMaterno", aMaterno);
+                        cmd.Parameters.AddWithValue("@Nombres", nombres);
+                        cmd.Parameters.AddWithValue("@Usuario", usuario);
+                        cmd.Parameters.AddWithValue("@CodCarrera", codCarrera);
+
+                        // Ejecutar
+                        conexion.Open();
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        if (filasAfectadas > 0)
+                        {
+                            MessageBox.Show("Alumno actualizado correctamente.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontró un alumno con ese código.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al actualizar el alumno: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                //Generar la consulta
+                SqlCommand comando = new SqlCommand("spListarAlumno", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+                //traer los datos de sql server en un contenedor 
+                SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dgvResultado.DataSource = table;
+            }
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            {
+                // Recoger los valores de los TextBox
+                string codAlumno = TBCodigo.Text;
+                string aPaterno = TBPaterno.Text;
+                string aMaterno = TBMaterno.Text;
+                string nombres = TBNombre.Text;
+                string usuario = TBUsuario.Text;
+                string codCarrera = TB.Text;
+
+                // Definir la consulta para ejecutar el procedimiento almacenado
+                string consulta = "spAgregarAlumno";
+
+                try
+                {
+                    // Crear la conexión y el comando
+                    using (SqlConnection conexion = new SqlConnection(cadena))
+                    {
+                        SqlCommand cmd = new SqlCommand(consulta, conexion);
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Agregar los parámetros necesarios para el procedimiento almacenado
+                        cmd.Parameters.AddWithValue("@CodAlumno", codAlumno);
+                        cmd.Parameters.AddWithValue("@APaterno", aPaterno);
+                        cmd.Parameters.AddWithValue("@AMaterno", aMaterno);
+                        cmd.Parameters.AddWithValue("@Nombres", nombres);
+                        cmd.Parameters.AddWithValue("@Usuario", usuario);
+                        cmd.Parameters.AddWithValue("@CodCarrera", codCarrera);
+
+                        // Abrir la conexión y ejecutar el comando
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();  // Ejecutar el procedimiento almacenado
+
+                        // Mostrar un mensaje de éxito
+                        MessageBox.Show("Alumno agregado correctamente.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // En caso de error, mostrar un mensaje
+                    MessageBox.Show("Error al agregar el alumno: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            {
+                // Obtener el código del alumno desde el TextBox
+                string codAlumno = TBCodigo.Text;
+
+                // Nombre del procedimiento almacenado
+                string consulta = "spEliminarAlumno";
+
+                try
+                {
+                    using (SqlConnection conexion = new SqlConnection(cadena))
+                    {
+                        SqlCommand cmd = new SqlCommand(consulta, conexion);
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Agregar parámetro del código de alumno
+                        cmd.Parameters.AddWithValue("@CodAlumno", codAlumno);
+
+                        // Abrir conexión y ejecutar
+                        conexion.Open();
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        // Verificar si se eliminó
+                        if (filasAfectadas > 0)
+                        {
+                            MessageBox.Show("Alumno eliminado correctamente.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontró un alumno con ese código.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar el alumno: " + ex.Message);
+                }
+            }
+        }
+    }
+}
